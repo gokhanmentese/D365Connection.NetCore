3c152355a15b396c1f13a01a628ab0e1be190aec
Gökhan Menteşe <gkhnmnts@gmail.com>
2022-04-05 15:48:43 +0300

    Add project files.

---
+++ D365Connection.NetCore/CRMHelper/CrmServiceSingleton.cs
@@ -0,0 +1,77 @@
+﻿using CrmService.AppSetting;
+using Microsoft.PowerPlatform.Dataverse.Client;
+using Microsoft.Xrm.Sdk;
+using System;
+
+namespace CrmService.CrmHelper
+{
+    public class CrmServiceSingleton : IDisposable
+    {
+        private static CrmServiceSingleton _serviceInstance;
+        private ServiceClient _organizationService;
+        private Guid _controlID;
+        private static object _lockObject = new object();
+
+        public IOrganizationService OrganizationService
+        {
+            get { return _organizationService; }
+        }
+
+        public Guid ControlID
+        {
+            get { return _controlID; }
+        }
+
+        private CrmServiceSingleton(Guid controlID)
+        {
+            _organizationService = GetCrmServiceAppUser();
+            _controlID = controlID;
+        }
+
+        public static CrmServiceSingleton GetService()
+        {
+            try
+            {
+                if (_serviceInstance == null)
+                {
+                    lock (_lockObject)
+                    {
+                        if (_serviceInstance == null)
+                            _serviceInstance = new CrmServiceSingleton(Guid.NewGuid());
+                    }
+                }
+
+                return _serviceInstance;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private static ServiceClient GetCrmServiceAppUser()
+        {
+            try
+            {
+                var organizationUri = appKeys.GetOrganizationUri;
+                var clientId = appKeys.GetClientId;
+                var clientSecret = appKeys.GetClientSecret;
+
+                var _CrmConnectionString = $@"AuthType=ClientSecret;url={organizationUri};ClientId={clientId};ClientSecret={clientSecret}";
+                var crmConn = new ServiceClient(_CrmConnectionString);
+             
+                return crmConn;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Dispose()
+        {
+            _organizationService = null;
+            _serviceInstance = null;
+        }
+    }
+}
---
+++ D365Connection.NetCore/Program.cs
@@ -0,0 +1,24 @@
+﻿using CrmService.CrmHelper;
+using Microsoft.Crm.Sdk.Messages;
+using System;
+
+namespace D365Connection.NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var crmServiceSingleton = CrmServiceSingleton.GetService();
+            var crmService = crmServiceSingleton.OrganizationService;
+
+            if (crmService == null)
+                System.Console.WriteLine("No Connected to Organization Service!");
+
+            WhoAmIRequest request = new WhoAmIRequest();
+            WhoAmIResponse response = (WhoAmIResponse)
+            crmService.Execute(request);
+
+            var userId = response.UserId;
+        }
+    }
+}
